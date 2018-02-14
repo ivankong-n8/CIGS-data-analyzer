865449eb172b82411a0842776457fa3881c06e64
ivank@mac <36361842+ivankong-n8@users.noreply.github.com>
2018-02-14 13:28:34 +0800

    show the group_label splitted

--- 20180210-Select folder.py
+++ 20180210-Select folder.py
@@ -24,6 +24,14 @@ window = tk.Tk()
 window.title('my window')
 window.geometry('200x200')
 
+
+# =============================================================================
+# global variables
+# =============================================================================
+data_set = None
+group_lable = None
+
+
 # =============================================================================
 # select _folder
 # =============================================================================
@@ -47,18 +55,19 @@ lb_folder.pack()
 # =============================================================================
 #  load folder
 # =============================================================================
-def load_folder(folder_name_tk,data_set,sample_list_Text):
+def load_folder(folder_name_tk,sample_list_Text):
     '''
     init data from data folder
     '''
+    global data_set 
     data_set = None
     foldername = folder_name_tk.get()
     data_set = data_sets_class.PV_series_data(foldername)
     sample_list_Text.insert(1.0,data_set.__str__())
     
-data_set = None
+
 b_load_folder = tk.Button(window, text= 'Load folder', command = 
-               lambda: load_folder(folder_name_tk,data_set,sample_list_Text))
+               lambda: load_folder(folder_name_tk,sample_list_Text))
 b_load_folder.pack()
 
 sample_list_Text = tk.Text(window, bg = 'Yellow',width = 200,height = 2)
@@ -73,11 +82,18 @@ label_input.pack()
 
 def input_labels(label_input,grouplb_tk):
     '''
-    
+    sep by '/'
     '''
-    grouplb_tk.set(label_input.get("1.0",'end-1c'))
+    global group_label 
+    group_label = None
+    context = label_input.get("1.0",'end-1c')
+    group_label = context.split('/')
+    str_show = ''
+    for label in group_label:
+        str_show += (label+'|')      
+    grouplb_tk.set(str_show)
     
-
+group_label = None
 b3 = tk.Button(window, text= 'Input group labels(sep by:/)', 
                command = lambda: input_labels(label_input,grouplb_tk))
 b3.pack()
@@ -86,21 +102,24 @@ grouplb_tk = tk.StringVar()
 lb_grouplb = tk.Label(window,textvariable = grouplb_tk, bg = 'Green',width = 200)
 lb_grouplb.pack()
 
-def analyzing(folder_name_tk,grouplb_tk):
+
+# =============================================================================
+# Do the analyzing
+# =============================================================================
+def analyzing(data_set,goup_label):
     '''
     folder_name_tk: tk.StringVar()
     grouplb_tk:
     function: init data_sets_class and plot graph
     '''
-    group_label = grouplb_tk.get().split('/')
+    print(data_set)
     print(group_label)
     data_set.sort(group_label)
-    print(data_set)
     data_set.get_boxplot()
     
     
 b2 = tk.Button(window, text= 'Analyze', 
-               command = lambda: analyzing(folder_name_tk,grouplb_tk))
+               command = lambda: analyzing(data_set,group_label))
 b2.pack()
 
 window.mainloop()
